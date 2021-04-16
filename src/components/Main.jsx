239a0797d88803e52a76bd69967a65c19d25a49c
meraj-tariq <meraj.tarique1618@gmail.com>
2021-04-16 15:52:26 -0700

    create context api project

---
+++ src/components/Main.jsx
@@ -0,0 +1,22 @@
+import React, { useContext } from 'react'
+import ThemeContext from '../Context/ThemeContext';
+import AppTheme from '../theme/colors';
+
+export default function Main() {
+    const theme = useContext(ThemeContext)[0];
+    const currentTheme = AppTheme[theme];
+    console.log(theme, currentTheme, "THEME")
+    return (
+        <main style={{
+            padding: "1rem",
+            backgroundColor: `${currentTheme.backgroundColor}`,
+            color: `${currentTheme.textColor}`,
+
+        }}>
+            <h1>Heading 1</h1>
+            <p>This is a paragraph</p>
+            <button> This is a button</button>
+        </main>
+    )
+}
+// export default Main;
